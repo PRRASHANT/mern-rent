98fe659937f2f0cb78e6386176f13e059cd637c6
PRRASHANT <prashant.e.kr@gmail.com>
2024-06-27 20:32:52 +0530

    install react,css and vite

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  content: [
+    './index.html', // Include the HTML file in the root directory
+    './src/**/*.{html,js,jsx,ts,tsx}', // Include all HTML, JS, JSX, TS, and TSX files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
