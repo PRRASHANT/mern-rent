e8ed8c6e2bdfa3b901c0bc9275725c0f3fca2e92
PRRASHANT <prashant.e.kr@gmail.com>
2024-07-06 06:19:39 +0530

    done

--- api/index.js
+++ api/index.js
@@ -1,39 +1,51 @@
-import express, { request } from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
-
 mongoose
-   .connect(process.env.Mango_URL).then(()=> {
-   console.log('connected to MangoDB')
-}).catch((err) => {
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
     console.log(err);
- })
+  });
 
+  const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000,()=> {
-    console.log('server is running on port 3000');
-    }
-);
+app.use(cookieParser());
 
-app.use("/api/user", userRouter);
-app.use('/api/auth', authRouter);
-
-app.use((err, req, res, next )=>{
-    const satusCode =err.satusCode || 500;
-    const message =err.message || 'Internal servor error';
-    return res.status(satusCode).json({
-        success: false,
-        satusCode,
-        message,
-
-    });
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
